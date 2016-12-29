d510187d4756597913141a001c20c56a8c8db61f
cphukan <chandan.phukan@gmail.com>
2016-12-28 22:18:51 -0500

    Added new coinMatch method

--- Testing/src/org/python/test/PythonTest.py
+++ Testing/src/org/python/test/PythonTest.py
@@ -33,8 +33,71 @@ class PythonTest(object):
                 line.append(previous_line[i] + previous_line[i+1])
             line += [1]
         return line
-
-
+    
+    def reverse(self, s):
+        
+        if len(s) <= 1:
+            return s
+        
+        return self.reverse(s[1:]) + s[0]
+    
+    def permute(self, s):
+        
+        out = []
+        if len(s) <= 1:
+            out = [s]
+            
+        else:
+            
+            for i,let in enumerate(s):
+                
+                for perm in self.permute(s[:i] + s[i+1:]):
+                    out += [let + perm]
+                    
+        return out 
+    
+    def coinMatch(self,target,coins):
+        
+        min_coin = target
+        
+        if target in coins:
+            return 1
+        
+        else:
+            
+            for i in [c for c in coins if c <= target]:
+                num_coins = 1 + self.coinMatch(target - i, coins)
+                
+                if num_coins < min_coin:
+                    min_coin = num_coins
+        
+        return min_coin
+    
+    def coinMatchDynamic(self, target, coins, known_results):
+        
+        min_coins = target
+        
+        if target in coins:
+            known_results[target]= 1
+            return 1
+        
+        elif known_results[target] > 0:
+            #print known_results[target]
+            return known_results[target]
+        
+        else:
+            for i in [ c for c in coins if c <= target]:
+                
+                num_coins = 1 + self.coinMatchDynamic(target - i, coins, known_results)
+                
+                #print min_coins
+                #print known_results[target]
+                if num_coins < min_coins:
+                    min_coins = num_coins
+                    known_results[target]= min_coins
+                    
+        return min_coins 
+        
 if __name__ == "__main__":
     
     obj = PythonTest();
@@ -48,4 +111,18 @@ if __name__ == "__main__":
         
     #print obj.factorial(n) 
     
-    print obj.pascal(n)
+    #print obj.pascal(n)
+    
+    #print obj.reverse("Hello World")
+    
+    #print obj.permute("abc")
+    
+    #print obj.coinMatch(27, [1,5,10])
+    
+    target = 74
+    coins = [1,5,10,25]
+    known_results = [0]*(target+1)
+    
+    print obj.coinMatchDynamic(target, coins, known_results)
+    
+    
